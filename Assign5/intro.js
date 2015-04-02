f934a17b16e409000589b10f90e74f883b1eaab0
Alijah <AlijahG@alijahs-mbp.newschool.edu>
2015-04-01 21:10:33 -0400

    add

---
+++ Assign5/intro.js
@@ -0,0 +1,14 @@
+
+var name = prompt("what is your name?"); 
+var birthday = prompt("When is your birthday?");
+var color = prompt("What is your favorite color?"); 
+
+var xholder = document.getElementById("xholder").innerHTML = name;
+ 
+
+
+document.getElementById("xholder").innerHTML = name;
+document.getElementById("xholder2").innerHTML = birthday;
+document.getElementById("xholder3").innerHTML = color;
+
+document.body.style.backgroundColor= color;
