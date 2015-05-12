9046ae50ec01eb7771e97f839f731c2ae5046263
Alijah <AlijahG@Alijahs-MBP.wireless.newschool.edu>
2015-05-12 00:18:30 -0400

    add

---
+++ Assign6/index6.js
@@ -0,0 +1,18 @@
+var Mywords = {
+word1,
+word2,
+word3,
+word4,
+word5,
+Function: function()
+Mywords.word1 = prompt ("adjective");
+Mywords.word2 = prompt ("animal");
+Mywords.word3 = prompt ("place");
+Mywords.word4 = prompt ("verb");
+Mywords.word5 = prompt ("");
+}
+
+
+document.getElementById("madlib").innerHTML
+
+    }
